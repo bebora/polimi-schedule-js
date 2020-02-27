dbea5778600578fd3c77e0b5b7ce2fe094a560e8
bebora <simone.rizzo@live.com>
2020-02-27 22:54:15 +0100

    Fix parsing for annual courses

--- scripts/schedule-to-ical.js
+++ scripts/schedule-to-ical.js
@@ -1,70 +1,83 @@
 let ICS = this["ics-js"];
 let weekdays = ["Domenica", "Lunedì", "Martedì", "Mercoledì", "Giovedì", "Venerdì", "Sabato"];
 
+function parseCourse(course) {
+  let events = [];
+  let noLessonTest = /\s*L'orario non è stato definito/;
+  let noScheduleTest = /\s*Nessun orario definito/;
+  if (noLessonTest.test(course) || noScheduleTest.test(course)) {
+    return [];
+  }
+  let titleMatch = /(\d{6}) - (.*?)(?:\s*\(Docente:.*\)|$)/m.exec(course);
+  let courseName = titleMatch[2];
+  let datesRegex = /(1|2|A|Annual(?:e*))\s*Inizio lezioni: (\d{2}\/\d{2}\/\d{4}) Fine lezioni: (\d{2}\/\d{2}\/\d{4})/g;
+  let datesGroups = [...course.matchAll(datesRegex)];
+  // Some annual courses consist of two courses, but the main course heading does not have any relevant event data and should be removed from the parsing.
+  if (datesGroups.length > 1) {
+    return parseCourse(course.split("\n\n").slice(1).join("\n\n")); //
+  }
+  else if (datesGroups.length === 1) {
+    let datesMatch = datesGroups[0];
+    let start = new Date(datesMatch[2].replace(/(\d{2})\/(\d{2})\/(\d{4})/,"$3-$2-$1"));
+    let end = new Date(datesMatch[3].replace(/(\d{2})\/(\d{2})\/(\d{4})/,"$3-$2-$1"));
+    let courseDays = /[^\n]+\n[^\n]+\n?([\s\S]*)/.exec(course);
+    if (courseDays[1] !== "") {
+      let rows = courseDays[1].trim().split("\n");
+      for (let j of rows) {
+        let timeMatch = /([^\s]*) dalle (\d{2}):(\d{2}) alle (\d{2}):(\d{2})/.exec(j);
+        let noRoomTest = /.*? Aula al momento non disponibile.*/;
+        let weekDay = weekdays.indexOf(timeMatch[1]);
+        let firstDay = new Date(start);
+        if (weekDay < start.getDay()) {
+          firstDay.setDate(firstDay.getDate()-firstDay.getDay()+7+weekDay);
+        }
+        else {
+          firstDay.setDate(firstDay.getDate()-firstDay.getDay()+weekDay);
+        }
+        firstDay.setHours(timeMatch[2], timeMatch[3], 0);
+        let lastDay = new Date(end);
+        if (weekDay <= end.getDay()) {
+          lastDay.setDate(lastDay.getDate()-lastDay.getDay()+weekDay);
+        }
+        else {
+          lastDay.setDate(lastDay.getDate()-lastDay.getDay()-7+weekDay);
+        }
+        let firstEnd = new Date(firstDay);
+        firstEnd.setHours(timeMatch[4], timeMatch[5], 0);
+        let location = null;
+        if (!noRoomTest.test(j)) {
+          let roomMatch = /aula (.*)/.exec(j);
+          location = roomMatch[1];
+        }
+        events.push(
+          {
+            "summary": courseName,
+            "start": firstDay,
+            "end": firstEnd,
+            "duration": timeMatch[4]-timeMatch[2], //TODO duration is redundant, check ical validity using only dtstart and dtend
+            "dtstamp": new Date(),
+            "location": location,
+            "rrule": "FREQ=WEEKLY;UNTIL="+lastDay.getFullYear()+("0"+(lastDay.getMonth()+1)).slice(-2)+("0"+lastDay.getDate()).slice(-2)+"T235959Z"
+          }
+        );
+      }
+    }
+  }
+  return events;
+}
+
 function parseText(allCourses) {
   try {
-    const separator = allCourses.includes("\n\n\n\n") ? "\n\n\n\n" : "\n\n\n"; 
+    const separator = "\n\n\n"; 
     let coursesList = allCourses.trim().split(separator);
     let events = [];
     for (let wholeCourse of coursesList) {
-      let course = wholeCourse.split("\n\n\n")[0];
-      let noLessonTest = /\s*L'orario non è stato definito/;
-      let noScheduleTest = /\s*Nessun orario definito/;
-      if (noLessonTest.test(course) || noScheduleTest.test(course)) {
-        continue;
-      }
-      let titleMatch = /(\d{6}) - (.*?)(?:\s*\(Docente:.*\)|$)/m.exec(course);
-      let courseName = titleMatch[2];
-      let datesMatch = /Inizio lezioni: (\d{2}\/\d{2}\/\d{4}) Fine lezioni: (\d{2}\/\d{2}\/\d{4})/.exec(course);
-      let start = new Date(datesMatch[1].replace(/(\d{2})\/(\d{2})\/(\d{4})/,"$3-$2-$1"));
-      let end = new Date(datesMatch[2].replace(/(\d{2})\/(\d{2})\/(\d{4})/,"$3-$2-$1"));
-      let courseDays = /[^\n]+\n[^\n]+\n?([\s\S]*)/.exec(course);
-      if (courseDays[1] !== "") {
-        let rows = courseDays[1].trim().split("\n");
-        for (let j of rows) {
-          let timeMatch = /([^\s]*) dalle (\d{2}):(\d{2}) alle (\d{2}):(\d{2})/.exec(j);
-          let noRoomTest = /.*? Aula al momento non disponibile.*/;
-          let weekDay = weekdays.indexOf(timeMatch[1]);
-          let firstDay = new Date(start);
-          if (weekDay < start.getDay()) {
-            firstDay.setDate(firstDay.getDate()-firstDay.getDay()+7+weekDay);
-          }
-          else {
-            firstDay.setDate(firstDay.getDate()-firstDay.getDay()+weekDay);
-          }
-          firstDay.setHours(timeMatch[2], timeMatch[3], 0);
-          let lastDay = new Date(end);
-          if (weekDay <= end.getDay()) {
-            lastDay.setDate(lastDay.getDate()-lastDay.getDay()+weekDay);
-          }
-          else {
-            lastDay.setDate(lastDay.getDate()-lastDay.getDay()-7+weekDay);
-          }
-          let firstEnd = new Date(firstDay);
-          firstEnd.setHours(timeMatch[4], timeMatch[5], 0);
-          let location = null;
-          if (!noRoomTest.test(j)) {
-            let roomMatch = /aula (.*)/.exec(j);
-            location = roomMatch[1];
-          }
-          events.push(
-            {
-              "summary": courseName,
-              "start": firstDay,
-              "end": firstEnd,
-              "duration": timeMatch[4]-timeMatch[2], //TODO duration is redundant, check ical validity using only dtstart and dtend
-              "dtstamp": new Date(),
-              "location": location,
-              "rrule": "FREQ=WEEKLY;UNTIL="+lastDay.getFullYear()+("0"+(lastDay.getMonth()+1)).slice(-2)+("0"+lastDay.getDate()).slice(-2)+"T235959Z"
-            }
-          );
-        }
-      }
+      events = [...events, ...parseCourse(wholeCourse.trim())];
     }
     return events;
   }
   catch(err) {
-    console.log("The website is no longer compatible with polimi-schedule-js. ("+err.message+")");
+    console.log("The website is no longer compatible with polimi-schedule-js. Please report the error message and the calendar text you are trying to convert. ("+err.message+")");
   }
   return null;
 }
