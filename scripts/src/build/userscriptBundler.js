2c63419e27b14248bea746118214dd8cdbea64ca
bebora <simone.rizzo@live.com>
2023-01-31 16:07:42 +0100

    Bump dependencies and fix typo

--- scripts/src/build/userscriptBundler.js
+++ scripts/src/build/userscriptBundler.js
@@ -6,7 +6,7 @@ const __dirname = path.dirname(__filename);
 
 const metadata = `// ==UserScript==
 // @name     polimi-schedule-js
-// @version  2.0.1
+// @version  ${process.env.npm_package_version}
 // @match    https://www4.ceda.polimi.it/manifesti/manifesti/controller/GestioneCarrelloPublic.do*
 // @match    https://www13.ceda.polimi.it/oralez/oralez/controller/MainDesktop.do*
 // @grant    none
